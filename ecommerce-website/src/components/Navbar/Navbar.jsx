5d8f676ee54ccdd9ffccdd894d62ae6f4e45b96a
Utkarsh Pandey <utk2665@gmail.com>
2024-01-06 22:41:51 +0530

    Worked on Navbar, more work needed

--- ecommerce-website/src/components/Navbar/Navbar.jsx
+++ ecommerce-website/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,34 @@
 import React from 'react'
 import './Navbar.css'
+import logo from '../assets/logo.png'
+import cart_icon from '../assets/cart_icon.png'
 
-export const Navbar = () => {
+
+const Navbar = () => {
+
+    const [menu, setMenu] = useState("Shop");
+
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+      <div className='nav-logo'>
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+
+      <ul className='nav-menu'>
+        <li onClick={()=>{setMenu("shop")}}>Shop</li>
+        <li onClick={()=>{setMenu("men")}}>Men</li>
+        <li onClick={()=>{setMenu("women")}}>Women</li>
+        <li onClick={()=>{setMenu("kids")}}>Kids</li>
+      </ul>
+
+      <div className='nav-login-cart'>
+        <button>Login</button>
+        <img src={cart_icon} alt="" /> 
+        <div className="nav-cart-count">0</div>
+      </div>
+    </div>
   )
 }
+
+export default Navbar
