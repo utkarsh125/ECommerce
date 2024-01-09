261368a44a38e93e7fd1a79a710364527072e6ed
Utkarsh Pandey <utk2665@gmail.com>
2024-01-09 19:48:44 +0530

    Reinstalled React Router DOM

--- ecommerce-website/src/App.jsx
+++ ecommerce-website/src/App.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ShopCategory from './pages/ShopCategory.jsx';
+import LoginSignup from './pages/LoginSignup.jsx';
 const App = () => {
   return (
     <div className=''>
+      <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path='/' element = {<Shop/>}></Route>
+        <Route path='/men' element = {<ShopCategory category="men"/>}></Route>
+        <Route path='/women' element = {<ShopCategory category="women"/>}></Route>
+        <Route path='/kids' element = {<ShopCategory category="kids"/>}></Route>
+        <Route path="product" element={<Product />}>
+          <Route path=':productId' element={<Product />} />
+        </Route>
+
+        <Route path='/cart' element={<Cart />}/>
+        <Route path='/cart' element={<LoginSignup />}/>
+
+        
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
